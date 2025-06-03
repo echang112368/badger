d33230ae2edd83103478d29c9e12719995f319fa
echang112368 <echang368@gmail.com>
2025-06-03 10:47:59 -0700

    Webhook is added

--- collect/urls.py
+++ collect/urls.py
@@ -2,9 +2,12 @@
 from django.urls import path
 from .views import redirect_view
 from .views import webhook_view
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('webhook/', webhook_view, name = "webhook_view"),
     path('<str:short_code>/', redirect_view, name='redirect_view'),
-   # path('webhook/', webhook_view, name='webhook_view'),
+   
 
 ]
