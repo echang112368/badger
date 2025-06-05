5042c3ead3904303b238f3e450af8775f78bc5c9
echang112368 <echang368@gmail.com>
2025-06-04 19:25:51 -0700

    updates to accounts app

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class CustomLoginForm(AuthenticationForm):
     username = forms.CharField()
     password = forms.CharField(widget = forms.PasswordInput)
-    
+
+class BusinessSignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", 'email', 'password1', 'password2')
+
+class CreatorSignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
