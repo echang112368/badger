de30073bb15fca5b2df8e7e70209cb2fc13c4e4f
echang112368 <echang368@gmail.com>
2025-07-22 23:33:06 -0700

    Add UUID for merchants

--- merchants/admin.py
+++ merchants/admin.py
@@ -5,5 +5,5 @@ from .models import MerchantMeta
 
 @admin.register(MerchantMeta)
 class MerchantMetaAdmin(admin.ModelAdmin):
-    list_display = ('user', 'company_name', 'business_ID')
-    search_fields = ('user__username', 'company_name', 'business_ID')
+    list_display = ('user', 'company_name', 'uuid')
+    search_fields = ('user__username', 'company_name', 'uuid')
