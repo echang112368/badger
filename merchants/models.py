6e4e0656a383f9d516bdd15a26a79fd108685251
echang112368 <echang368@gmail.com>
2025-07-26 22:56:09 -0700

    Treat webhook IDs as strings

--- merchants/models.py
+++ merchants/models.py
@@ -8,13 +8,6 @@ class MerchantMeta(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     affiliate_percent = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
-    @property
-    def int_uuid(self):
-        """Return the UUID as an integer with dashes removed."""
-        try:
-            return int(str(self.uuid).replace("-", ""))
-        except (TypeError, ValueError):
-            return None
 
     def __str__(self):
         return self.company_name
