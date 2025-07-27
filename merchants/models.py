1ddf559f28285b407fc0f7506fb7fbb6fd1b9ad5
echang112368 <echang368@gmail.com>
2025-07-26 18:13:16 -0700

    Add financial ledger system

--- merchants/models.py
+++ merchants/models.py
@@ -6,9 +6,15 @@ class MerchantMeta(models.Model):
     user = models.OneToOneField('accounts.CustomUser', on_delete=models.CASCADE)
     company_name = models.CharField(max_length=255, blank=True)
     uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    affiliate_percent = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
     def __str__(self):
         return self.company_name
+
+    @property
+    def balance(self):
+        from ledger.models import LedgerEntry
+        return LedgerEntry.merchant_balance(self.user)
 
 class MerchantItem(models.Model):
     merchant = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
