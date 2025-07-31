0b7707134784bb514a69e3fb550d53a26b00aeff
echang112368 <echang368@gmail.com>
2025-07-30 22:35:29 -0700

    updates

--- random_links/settings.py
+++ random_links/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1@#i+ivuv)%n68yqzwzg%ggqdzfqe9j$@gan+^0)!0e3%3^0x2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", 'd2789eda05b1.ngrok-free.app']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", '17501b0f944d.ngrok-free.app']
 
 
 # Application definition
@@ -141,6 +141,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'webmaster@localhost'
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://d2789eda05b1.ngrok-free.app"
+    "https://17501b0f944d.ngrok-free.app"
 
 ]
