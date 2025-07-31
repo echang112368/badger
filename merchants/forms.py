d2e70196782cb81698f1fa6f98242144afc6cb88
echang112368 <echang368@gmail.com>
2025-07-30 22:43:55 -0700

    Add PayPal email field note on merchant dashboard

--- merchants/forms.py
+++ merchants/forms.py
@@ -12,5 +12,8 @@ class MerchantMetaForm(forms.ModelForm):
     class Meta:
         model = MerchantMeta
         fields = ["affiliate_percent", "paypal_email"]
-        labels = {"affiliate_percent": "Commission Rate (%)", "paypal_email": "PayPal Email"}
+        labels = {
+            "affiliate_percent": "Commission Rate (%)",
+            "paypal_email": "PayPal Email (for invoices)",
+        }
 
