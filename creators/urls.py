b7a89990e584cde6905e85eb507a7f8b005ed1e3
echang112368 <echang368@gmail.com>
2025-08-02 16:19:00 -0700

    Split creator dashboard into routed pages

--- creators/urls.py
+++ creators/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import creator_dashboard, respond_request
+from .views import (
+    creator_dashboard,
+    creator_earnings,
+    creator_affiliate_companies,
+    creator_my_links,
+    creator_settings,
+    creator_support,
+    respond_request,
+)
 
 urlpatterns = [
     path('dashboard/', creator_dashboard, name='creator_dashboard'),
+    path('earnings/', creator_earnings, name='creator_earnings'),
+    path('affiliate-companies/', creator_affiliate_companies, name='creator_affiliate_companies'),
+    path('my-links/', creator_my_links, name='creator_my_links'),
+    path('settings/', creator_settings, name='creator_settings'),
+    path('support/', creator_support, name='creator_support'),
     path('respond-request/<int:link_id>/', respond_request, name='respond_request'),
 ]
