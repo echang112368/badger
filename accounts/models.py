6b0c5472e43e54fa6223f253d1d3942c0915c9df
echang112368 <echang368@gmail.com>
2025-08-13 13:50:24 -0700

    Revert "Add UUID field to CustomUser model"

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,6 @@
-import uuid
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
     is_merchant = models.BooleanField(default = False)
     is_creator = models.BooleanField(default = False)
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
