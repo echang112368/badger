045195ac5708bbf93f8fb78a2ff32a06758d12c0
echang112368 <echang368@gmail.com>
2025-08-12 22:55:37 -0700

    Add UUID field to CustomUser

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+import uuid
 
-# Create your tests here.
+
+class CustomUserModelTests(TestCase):
+    def test_uuid_is_created_and_unique(self):
+        User = get_user_model()
+        user1 = User.objects.create_user(username="user1", password="testpass123")
+        user2 = User.objects.create_user(username="user2", password="testpass123")
+
+        self.assertIsNotNone(user1.uuid)
+        self.assertIsInstance(user1.uuid, uuid.UUID)
+        self.assertIsNotNone(user2.uuid)
+        self.assertNotEqual(user1.uuid, user2.uuid)
