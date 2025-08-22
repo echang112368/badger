d1194284c07c300a216bca0c78731d2c950e3dc4
echang112368 <echang368@gmail.com>
2025-08-21 20:38:34 -0400

    updates

--- shopify_app/management/commands/inject_scripts_all_merchants.py
+++ shopify_app/management/commands/inject_scripts_all_merchants.py
@@ -4,8 +4,8 @@ from merchants.models import MerchantMeta
 from shopify_app.shopify_client import ShopifyClient
 
 SCRIPT_SRCS = [
-    "https://6d464edfb053.ngrok-free.app/static/js/referral_tracker.js",
-    "https://6d464edfb053.ngrok-free.app/static/js/cart_attributes.js",
+    "https://5cb2cd6b55a0.ngrok-free.app/static/js/referral_tracker.js",
+    "https://5cb2cd6b55a0.ngrok-free.app/static/js/cart_attributes.js",
     
 ]
 
