e1e716a16c9aad1b5d5192d44f8d0ea62ec1703e
echang112368 <echang368@gmail.com>
2025-09-16 13:46:48 -0400

    updates

--- collect/views.py
+++ collect/views.py
@@ -74,7 +74,7 @@ def webhook_view(request):
                     if commission > 0:
                         LedgerEntry.objects.create(
                             creator=creator_meta.user,
-                            merchant=merchant_meta.user,
+
                             amount=commission,
                             entry_type="commission",
                         )
@@ -210,7 +210,6 @@ def orders_create_webhook(request):
         # Credit the content creator with the commission
         LedgerEntry.objects.create(
             creator=creator_meta.user,
-            merchant=merchant_meta.user,
             amount=commission_total,
             entry_type="commission",
         )
