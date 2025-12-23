3903a654b907022a9144d61a8ebd5316d52faa2a
echang112368 <echang368@gmail.com>
2025-12-22 18:45:24 -0800

    Revert "Refactor Shopify embedded auth and webhook flow"

--- shopify_app/urls.py
+++ shopify_app/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.embedded_app_home, name='shopify_embedded_home'),
-    path('shopify/install/', views.install, name='shopify_oauth_authorize'),
-    path('shopify/oauth/callback/', views.oauth_callback, name='shopify_oauth_callback'),
-    path('shopify/billing/return/', views.billing_return, name='shopify_billing_return'),
-    path('shopify/webhooks/', views.webhook_receiver, name='shopify_webhooks'),
-    path('shopify/discounts/<uuid:merchant_uuid>/', views.create_discount, name='create_discount'),
+    path('oauth/authorize/', views.oauth_authorize, name='shopify_oauth_authorize'),
+    path('oauth/callback/', views.oauth_callback, name='shopify_oauth_callback'),
+    path('billing/return/', views.billing_return, name='shopify_billing_return'),
+    path('create-discount/<uuid:merchant_uuid>/', views.create_discount, name='create_discount'),
 ]
