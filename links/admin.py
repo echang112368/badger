f39402d6bbf62439dd49b1b240229c5111981f4d
echang112368 <echang368@gmail.com>
2026-01-10 11:58:05 -0800

    Add partnership request workflow and marketplace updates

--- links/admin.py
+++ links/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MerchantCreatorLink
+from .models import MerchantCreatorLink, PartnershipRequest
 
 class CreatorInline(admin.TabularInline):
     model = MerchantCreatorLink
@@ -20,6 +20,13 @@ class MerchantCreatorLinkAdmin(admin.ModelAdmin):
     list_display = ('merchant', 'creator', 'status')
     search_fields = ('merchant__username', 'creator__username')
     list_filter = ('status',)
+
+
+@admin.register(PartnershipRequest)
+class PartnershipRequestAdmin(admin.ModelAdmin):
+    list_display = ("creator", "merchant", "status", "created_at")
+    search_fields = ("creator__username", "merchant__username", "message")
+    list_filter = ("status", "created_at")
 
 # Now in accounts/admin.py you can do:
 # from links.admin import CreatorInline, MerchantInline
