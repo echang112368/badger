f9af4e175ef9d05e3b96fa44c932bccfe27c67c8
echang112368 <echang368@gmail.com>
2026-01-10 14:21:18 -0800

    Add deterministic smart search API

--- creators/api_urls.py
+++ creators/api_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .api_views import CreatorNameView
+from .api_views import CreatorNameView, SearchAPIView
 
 urlpatterns = [
     path('<uuid:uuid>/', CreatorNameView.as_view(), name='creator_name_api'),
+    path('search/', SearchAPIView.as_view(), name='creator_search_api'),
 ]
