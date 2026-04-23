b48ef4c8e5f929c57111bc7d28404cf493dfeee3
echang112368 <echang368@gmail.com>
2026-04-23 14:35:29 -0400

    Add Instagram disconnect flow that clears synced data

--- instagram_connect/urls.py
+++ instagram_connect/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     connect_instagram,
     instagram_callback,
+    instagram_disconnect,
     instagram_status,
     instagram_sync,
 )
@@ -12,5 +13,6 @@ urlpatterns = [
     path("connect/", connect_instagram, name="connect_instagram"),
     path("callback/", instagram_callback, name="instagram_callback"),
     path("status/", instagram_status, name="instagram_status"),
+    path("disconnect/", instagram_disconnect, name="instagram_disconnect"),
     path("sync/", instagram_sync, name="instagram_sync"),
 ]
